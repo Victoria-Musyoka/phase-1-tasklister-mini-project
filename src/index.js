106ce95f57f7199b861bbba609f5ff1a2a5158b5
Victoria Musyoka <victoria.musyoka@student.moringaschool.com>
2022-06-15 17:56:00 +0300

    completing assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector('task-form')
+  form.addEventListener('submit', (event)=>{
+    event.preventDefault()
+      makeToDo(event.target.new_task_description.value)
+      form.reset()
+  })
 });
+
+function makeToDo(todo){
+  Let Li  = document.createElement('Li');
+  Let deletebtn = document.createElement('button');
+  deletebtn.addEventListener('click',deleteTodo)
+  deletebtn.textContent = 'done'
+  Li.textContent=`${todo}`;
+  Li.appendChild(deletebtn);
+  console.log(li)
+
+  document.querySelector('task').appendChild(li)
+}
+function deleteTodo(event){
+  event.target.parentNode.remover()
+}
